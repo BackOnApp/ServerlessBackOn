608e462e4a2c2f9ac4de65b1c9c00ba771fb4e88
BackOnApp <appbackon@gmail.com>
2020-04-11 01:36:56 +0200

    Lavorare fino alle 1:30 è controproducente...

--- api/deleteRequest.js
+++ api/deleteRequest.js
@@ -1,7 +1,9 @@
 const mongoInterface = require('../mongoInterface');
 const ObjectId = require('mongodb').ObjectId;
+const sendPush = require("public/sendPush.js")
 
 module.exports = (request, response) => {
+  console.log('È solo una prova...');
   let id = request.body._id;
   if (id == null) {
     console.error("_id field not found in request");
@@ -12,14 +14,22 @@ module.exports = (request, response) => {
   }
   mongoInterface.Task.deleteOne({_id: ObjectId(id)})
   .then(
-    () => {
-      response.send(200);
+    async (task) => {
+      if (task.helperID != null){
+        console.log('Ciao, sono io, Topolinno!');
+        await sendPush(task.helperID, 'A quanto pare per '.concat(task.title, 'il needer è diventato autosufficiente'));
+        console.log('Request removed!');
+        response.status(200).json({"result":"Request removed!"});
+    } else {
+      console.log('Sei scemo')
+      response.status(200).json({"message" : "Request removed successfully"});
     }
-  )
-  .catch(
+  }
+  ).catch(
     (error) => {
+      console.error("Error with removeRequest");
       console.error(error);
-      response.send(400);
+      response.status(400).json(error);
     }
   );
 };
